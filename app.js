9d2933219352bca5cea4785326c1d9afc1c66208
shandheep E <shandheepethiraj02@gmail.com>
2025-05-04 20:25:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const userRoutes = require('./userroutes.js');
+const userRoutes = require('userroutes.js');
 
 // Initialize express app
 const app = express();
